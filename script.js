8cbad4b3292867a1ee86f97dd94e433db62e75a5
naveen0904 <727722euec114@skect.ac.in>
2024-08-05 12:13:28 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function updateTime() {
+    const now = new Date();
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+  
+    hours = hours < 10 ? `0${hours}` : hours;
+    minutes = minutes < 10 ? `0${minutes}` : minutes;
+    seconds = seconds < 10 ? `0${seconds}` : seconds;
+  
+    const timeString = `${hours}:${minutes}:${seconds}`;
+    document.getElementById('time').textContent = timeString;
+  }
+    setInterval(updateTime, 1000);
+    updateTime();
